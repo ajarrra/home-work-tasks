ccb6b9ec0238ccc03264a515771f71b40e14a97c
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-17 14:58:28 +0100

    solved the problem where it was necessary Write a function that displays the volume of the cone according to the given parameters - height and radius

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function myName(name, surname) {
     console.log("My name is " + name, surname);
 }
 myName("Ajar", "Koshoeva");
+
+//Напишите функцию, которая выводит объем конуса по заданным параметрам - высота (height) и радиус (radius).
+let pi = 3.14;
+function coneVolume(height, radius){
+    console.log ("Объем конуса равен " + (height * pi * radius ** 2 ) / 3  );
+}
+coneVolume(7,6);
