cf349585f2151d694e67a6a219d5abad1c28b2f5
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-17 15:00:10 +0100

    changed the content in console.log

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ myName("Ajar", "Koshoeva");
 
 //Напишите функцию, которая выводит объем конуса по заданным параметрам - высота (height) и радиус (radius).
 let pi = 3.14;
-function coneVolume(height, radius){
-    console.log ("Объем конуса равен " + (height * pi * radius ** 2 ) / 3  );
+function coneVolume(height, radius) {
+    console.log("Объем конуса равен " + (height * pi * radius ** 2) / 3);
 }
-coneVolume(7,6);
+coneVolume(7, 6);
