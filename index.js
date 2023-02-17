a4fb325c4b857d916638d72eab4acfbeed3e0ba5
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-17 14:50:46 +0100

    wrote a function that displays my first name along with my last name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //1. Напишите функцию, которая выводит ваше имя и фамилию.
 
-function myName(name, surname){
-    console.log("My name is " +  name, surname );
+function myName(name, surname) {
+    console.log("My name is " + name, surname);
 }
-myName("Ajar" , "Koshoeva");
+myName("Ajar", "Koshoeva");
