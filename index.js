04d18b99998ff4563342d984c4d29dc2b997b868
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-19 18:17:08 +0100

    finished array of strings and concatenate them into one string

--- index.js
+++ index.js
@@ -112,3 +112,10 @@ for (let i = 0; i < example2.length; i++) {
 }
 console.log(num);
 
+//17. Создайте массив строк и объедините их в одну строку. // ["I", "like", "to", "eat!"]
+let example3 =["I", "like", "to", "eat!"];
+let union = "";
+for( let i = 0; i < example3.length; i++  ){
+    union = union + example3[i];
+}
+console.log (union);
