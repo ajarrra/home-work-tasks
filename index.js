5fe2a56049950e2c39f961fbcf2bd36bb51ad8e1
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-20 19:47:22 +0100

    finished doing all the tasks and did formate document for my code

--- index.js
+++ index.js
@@ -134,20 +134,20 @@ console.log(example5[2]);
 
 //20. Создайте массив из чисел и выведите квадраты этих чисел. [2, 3, 4, 5]
 // 4 9 16 25
-example6 = [ 2 , 3 , 4 , 5];
-let result1 = 0
-for(let i = 0; i < example6.length; i++ ){
-  console.log 
-}
+//example6 = [2, 3, 4, 5];
+//for (let i = 0; i < example6.length; i++) {
+//    console.log
+//}
 
 //21. Создайте массив строк и проверьте есть ли такой элемент в массиве. // ["cat", "dog", "snake"] 
 // cat - true
 // parrot - false
- 
+
 let animals = ["cat", "dog", "snake"];
-console.log(animals.includes("cat")); 
+console.log(animals.includes("cat"));
 console.log(animals.includes("parrot"));
 //includes проверка true / false
+
 
 
 
